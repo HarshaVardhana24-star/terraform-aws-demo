24bc3c8cbf8f6be265b36d7c41e27054541714d0
HarshaVardhana24-star <harshavardhana24@gmail.com>
2026-05-20 12:09:21 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,9 +1,12 @@
 terraform {
-  required_version = ">= 1.5.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
+  required_version = "1.15.3"
+
+  cloud {
+    
+    organization = "GL-terraform-training1"
+
+    workspaces {
+      name = "gl-workspace1"
     }
   }
 }
